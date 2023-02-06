eaab8d8af65e18499fd37992deba5ab780e5fedf
MorteritoKeloke <zoroarkb@gmail.com>
2023-02-06 10:09:02 +0100

    hoho

---
+++ Assets/Scripts/EX1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EX1 : MonoBehaviour
+{
+    private void Start()
+    {
+        for (int i = 100; i >= 1; i--)
+        {
+            Debug.Log(i);
+        }
+    }
+}
